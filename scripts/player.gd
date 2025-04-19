c903d9ed105d50cea2bcdca20bd80b6acc2afee8
JianiLi98 <jiani.li9988olv@gmail.com>
2025-04-19 20:38:07 +1000

    initial commit-priest able to move

---
+++ scripts/player.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+@export var speed := 150.0
+@onready var anim := $AnimatedSprite2D
+
+func _physics_process(delta):
+	var direction := Vector2.ZERO
+	direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	direction.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	direction = direction.normalized()
+	velocity = direction * speed
+
+	# directions
+	if direction != Vector2.ZERO:
+		if abs(direction.x) > abs(direction.y):
+			if direction.x > 0:
+				anim.play("walk_right")
+			else:
+				anim.play("walk_left")
+		else:
+			if direction.y > 0:
+				anim.play("walk_down")
+			else:
+				anim.play("walk_up")
+	else:
+		anim.stop()
+
+	move_and_slide()
